f55ef71ea484fed3fa3a2787e698eba0e5e8e2a6
Jake Carlson <jakecarlson@Jakes-MacBook-Pro.local>
2019-07-23 12:06:16 -0500

    new order of coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,42 @@
 $(document).ready(function () {
 
-    var crystals = {
-        blueCrystal:
-        {
-            color: "blue",
-            value: 0
-        },
-        greenCrystal:
-        {
-            color: "green",
-            value: 0
-        },
-        orangeCrystal:
-        {
-            color: "orange",
-            value: 0
-        },
-        redCrystal:
-        {
-            color: "red",
-            value: 0
-        },
-
-    };
-
-
-
-var targetNumber = 0;
-var counter = 0;
-
-var wins = 0;
-var losses = 0;
-
-console.log(counter)
-
 // 1- a number is generated when the page loads between 19-120, and each button is given a valueof 1 -12
+    // Numbered Arrays
+        // Random computer var. array
+        var rand = [];
+
+        for (var r = 19; r < 121; r++) {
+            rand.push(r);
+        }
+        console.log(rand); //--- it works!
+
+
+        // crystal number arrays
+        var crystals = [];
+        for (var c = 1; c < 13; c++) {
+            crystals.push(c);
+        }
+        console.log(crystals); //--- it works!
+
+    // Global variables
+        // random numbers/ var chosen by computer
+        var randNumber; 
+        var crystalNumbers = [];
+        var c1;
+        var c2;
+        var c3;
+        var c4;
+        console.log(crystalNumbers);
+        console.log(c1);
+        // users scores and guesses
+        var userScore = 0;
+        var wins = 0;
+        var losses = 0;
+        console.log(userScore);
 
 // 2- user clicks a button to see what value is, value is added to userGuess
+    // functions
+
 
 // 3- gave ends either of two ways
 
