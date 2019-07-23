89509f61651b5f941f1cae46f46f876947df37c7
Jake Carlson <jakecarlson@Jakes-MacBook-Pro.local>
2019-07-23 10:49:11 -0500

    created var for buttons/ crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,30 @@
+$(document).ready(function() {
+
+    var crystals = {
+        blueCrystal:
+        {
+            color: "blue",
+            value: 0
+        },
+        greenCrystal:
+        {
+            color: "green",
+            value: 0
+        },
+        orangeCrystal:
+        {
+            color: "orange",
+            value: 0
+        },
+        redCrystal:
+        {
+            color: "red",
+            value: 0
+        },
+
+    },
+
+})
 // 1- a number is generated when the page loads between 19-120, and each button is given a valueof 1 -12
 
 // 2- user clicks a button to see what value is, value is added to userGuess
@@ -9,3 +36,6 @@
 // b- total guessed = more than generated number, losses = +1
 
 // 4 - game restarts
+
+
+
