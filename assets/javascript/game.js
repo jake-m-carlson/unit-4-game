1972ddc4e4d5db8bab0f0c482d43403a5b5f921f
Jake Carlson <jakecarlson@Jakes-MacBook-Pro.local>
2019-07-23 10:23:17 -0500

    typed pseudo code onto game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// 1- a number is generated when the page loads between 19-120, and each button is given a valueof 1 -12
+
+// 2- user clicks a button to see what value is, value is added to userGuess
+
+// 3- gave ends either of two ways
+
+// a- total guessed = generated number, wins = +1
+
+// b- total guessed = more than generated number, losses = +1
+
+// 4 - game restarts
